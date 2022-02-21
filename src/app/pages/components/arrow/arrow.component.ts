20ace75535c3c4d10f3310d10df5817e694c55f2
duboisfacu <facundubois@hotmail.com>
2022-02-20 23:38:08 -0300

    estilos"

--- src/app/pages/components/arrow/arrow.component.ts
+++ src/app/pages/components/arrow/arrow.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ArrowComponent implements OnInit {
   @Input() href?: string;
   constructor() {}
-
+  closeNav(a: any) {
+    document.getElementById(a)?.scrollIntoView();
+  }
   ngOnInit(): void {}
 }
